dcbbe767dd9fa432b7bc5f83cb35c2fcb38c36af
osakana <n1290268@jn2.iwasaki.ac.jp>
2021-01-20 13:18:25 +0900

    Change PHP

--- chat.php
+++ chat.php
@@ -3,6 +3,7 @@
 
 	//index.phpから必要な情報を取得
 	$msg = $_POST['msg'];
+	$user = $_POST['user'];
 
 	//DB接続に必要な情報
 	$db = mysqli_connect('ホスト名', 'ユーザ名', 'パスワード', 'DB名');
@@ -10,7 +11,7 @@
 	
 	//DB接続, SQL文実行
 	try {
-		$sql = "INSERT INTO `chatlog2` (user, text, date) VALUES ('test03', '$msg', NOW())";
+		$sql = "INSERT INTO `chatlog2` (user, text, date) VALUES ('$user', '$msg', NOW())";
 		$result = mysqli_query($db, $sql);
 	} catch (Exception $e) {
 		echo $e->getMessage() . PHP_EOL;
