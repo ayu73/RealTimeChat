a47e9acc979bdb71087dd4cc7e1ebdb32261a0cb
osakana <n1290268@jn2.iwasaki.ac.jp>
2021-01-27 17:00:58 +0900

    change php,html,css

--- chat.php
+++ chat.php
@@ -1,23 +1,23 @@
 <?php
-	//index.phpで入力されたメッセージをDBに登録する
+	//index.htmlで入力されたメッセージをDBに登録する
 
-	//index.phpから必要な情報を取得
+	//index.htmlから必要な情報を取得
 	$msg = $_POST['msg'];
 	$user = $_POST['user'];
 
 	//DB接続に必要な情報
-	$db = mysqli_connect('ホスト名', 'ユーザ名', 'パスワード', 'DB名');
+	$db = mysqli_connect('hostname', 'username', 'password', 'dbname');
 	mysqli_set_charset($db, 'utf8');
 	
 	//DB接続, SQL文実行
 	try {
-		$sql = "INSERT INTO `chatlog2` (user, text, date) VALUES ('$user', '$msg', NOW())";
+		$sql = "INSERT INTO `chatlog` (user, text, date) VALUES ('$user', '$msg', NOW())";
 		$result = mysqli_query($db, $sql);
 	} catch (Exception $e) {
 		echo $e->getMessage() . PHP_EOL;
 	}
-
+	
 	//index.htmlに戻る
-	header('Location: index.htmlのURL');
-	exit;
+	header('Location: home.php');
+	exit;	
 ?>
